4cddd07c81010cd0b957489d57887d9b2b160fe7
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-09 13:46:23 +0200

    Updated according to review

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(10, 2)) == 10
+    func_result = split_integer(10, 2)
+    assert func_result == [5, 5]
+    assert sum(func_result) == 10
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
